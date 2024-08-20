6c810e5601918beeca0c12a6a8221bd9101e4544
abhishandy <abhishek.sandy@gmail.com>
2024-08-20 10:29:58 -0400

    setup Xcode project

---
+++ Easy Multisig/Easy Multisig/Easy_MultisigApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_MultisigApp.swift
+//  Easy Multisig
+//
+//  Created by Abhishek Shandilya on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Easy_MultisigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
